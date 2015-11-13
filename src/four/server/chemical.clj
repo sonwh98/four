160ecf208ac50e508b09044fad6ce53c0d67b4ba
sto <Son.C.To@gmail.com>
2015-11-12 20:54:00 -0500

    refactor: extract function get-elements

--- src/four/server/chemical.clj
+++ src/four/server/chemical.clj
@@ -4,8 +4,12 @@
             [four.transit :as t]
             [four.server.ws :as ws :refer [process-msg]]))
 
-(defmethod process-msg :get-elements [[websocket-channel [kw msg]]]
+(defn get-elements []
   (let [elements-edn-file (io/file (io/resource "public/elements.edn"))
         elements-edn [:elements (-> elements-edn-file slurp read-string)]
         transit-msg (t/serialize elements-edn)]
+    transit-msg))
+
+(defmethod process-msg :get-elements [[websocket-channel [kw msg]]]
+  (let [transit-msg (get-elements)]
     (ws/send websocket-channel transit-msg)))
